7db0a7aa189aa38bd1cf09ca523f8d91d30fd598
AndreaChiappini <117998815+AndreaChiappini@users.noreply.github.com>
2022-11-11 15:58:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
+float a,b,c;//variabili
 
 cin >> a >> b >> c;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+  cout<<0.5*a*b<<endl;// area triangolo
+  cout<<a*a<<endl;//area quadrato
+  cout<<a*b<<endl;// area rettangolo
+  cout<<((b+a)*c)*0.5<<endl; // area trapezio
+  
+return 0;
 }
